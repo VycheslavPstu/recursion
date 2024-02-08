86ee05a9724f94ea006990e5105de5c930399196
Вячеслав <148702947+VycheslavPstu@users.noreply.github.com>
2024-02-09 01:22:16 +0500

    Create hanoi_tower.cpp

---
+++ hanoi_tower.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+void hanoi_tower(int n, int from, int to, int tmp)
+{
+    if (n != 0)
+    {
+        hanoi_tower(n - 1, from, tmp, to);
+        printf("Переместил кольцо %d с %d на %d \n", n, from, to);
+        hanoi_tower(n - 1, tmp, to, from);
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Количество колец = ");
+    scanf("%d", &n);
+    hanoi_tower(n, 1, 3, 2);
+    return 0;
+}
+
